2f601cdc3856cd86462beba63244c7dddf3d2da6
Brittany Cowper <brittferren@gmail.com>
2023-07-21 21:16:08 -0500

    fetch appointment data from API

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import Loading from './Loading';
 import Panel from './Panel';
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
 
 import classnames from "classnames";
 
@@ -8,30 +15,33 @@ const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
 class Dashboard extends Component {
 
   state = {
-    loading: false,
-    focused: null
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
   };
 
   //Instance method
@@ -54,6 +64,19 @@ class Dashboard extends Component {
     if (focused) {
       this.setState({ focused });
     }
+    // request data from API and update state
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -76,7 +99,7 @@ class Dashboard extends Component {
         <Panel
           key={panelItem.id}
           label={panelItem.label}
-          value={panelItem.value}
+          value={panelItem.getValue(this.state)}
           onSelect={() => this.selectPanel(panelItem.id)}
         />
       ));
